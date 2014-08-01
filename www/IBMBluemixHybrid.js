13d549f32715a7572236ec5a7df77466b05c5eb2
System Administrator <root@hocotate.austin.ibm.com>
2014-08-01 11:46:05 -0500

    build changes

--- www/IBMBluemixHybrid.js
+++ www/IBMBluemixHybrid.js
@@ -4,7 +4,7 @@
  *  US Government Users Restricted Rights - Use, duplication or
  *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
  *
- *  IBM Mobile Cloud Services JavaScript SDK, v1.0.0.20140729-1224
+ *  IBM Mobile Cloud Services JavaScript SDK, v1.0.0.20140801-1135
  *
  */
 
@@ -3350,7 +3350,7 @@ define('ibm/mobile/_IBMBluemix', ['require', 'exports', 'module', './lib/IBMUnde
 
 var logger = ibmLogger.getLogger();
   var _IBMBluemix = {
-      VERSION: "1.0.0.20140729-1224",
+      VERSION: "1.0.0.20140801-1135",
       config: {},
       initialize: function (config) {
         logger.debug("IBMBluemix: initializing version: " + this.getVersion());
